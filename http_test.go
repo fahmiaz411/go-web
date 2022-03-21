a9464d118f2f4ca00c5ba8c7f65860538b4193ab
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-21 12:34:17 +0700

    request response simulation

--- http_test.go
+++ http_test.go
@@ -2,14 +2,26 @@ package goweb
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
-func HelloHandler(writer http.ResponseWriter, request http.Request) {
-	fmt.Fprintln(writer, "hello")
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "hello world")
 }
 
-// func TestHttp(t *testing.T){
-//   request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
-//   recorder := httptest.NewRecorder()
-// }
+func TestHttp(t *testing.T){
+  request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+  recorder := httptest.NewRecorder()
+
+  HelloHandler(recorder, request)
+
+  response := recorder.Result()
+  body, _ := io.ReadAll(response.Body)
+
+  bodyString := string(body)
+
+  fmt.Println(bodyString)
+}
