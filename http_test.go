e9df68e58f28687638348212f55e3736860aed82
Fahmi Aziz <fahmiaziz884@gmail.com>
2022-03-21 09:58:38 +0700

    http

--- http_test.go
+++ http_test.go
@@ -3,15 +3,13 @@ package goweb
 import (
 	"fmt"
 	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
 func HelloHandler(writer http.ResponseWriter, request http.Request) {
 	fmt.Fprintln(writer, "hello")
 }
 
-func TestHttp(t *testing.T){
-  request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
-  recorder := httptest.NewRecorder()
-}
+// func TestHttp(t *testing.T){
+//   request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+//   recorder := httptest.NewRecorder()
+// }
