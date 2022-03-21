de181691f06a6410734c6dd9bcfe8c9612854b3e
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-21 12:33:35 +0700

    static file with embed or no

---
+++ fileserver_test.go
@@ -0,0 +1,42 @@
+package goweb
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"testing"
+)
+
+func TestFileServer(t *testing.T) {
+	directory := http.Dir("./resources")
+	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+
+}
+
+//go:embed resources
+var resources embed.FS
+func TestFileServerEmbed(t *testing.T) {
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS((directory)))
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+
+}
