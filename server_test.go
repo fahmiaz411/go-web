7ec10497fa6b21aa08c523602851963e31f13c42
Fahmi Aziz <fahmiaziz884@gmail.com>
2022-03-21 09:58:58 +0700

    -

---
+++ server_test.go
@@ -0,0 +1,17 @@
+package goweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
